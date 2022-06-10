c678083bc1e4a35fadd2cc8c6488a3c33a317119
Bhargav <bhargav.bhavani@odaninfotech.com>
2022-06-10 11:26:46 +0530

    using var

--- a1.dart
+++ a1.dart
@@ -6,7 +6,18 @@
 // }
 
 //String interpolation
+// void main() {
+//   String a = 'Bhargav';
+//   print('String size is ${a.length}');
+// }
+
+//using var
 void main() {
-  String a = 'Bhargav';
-  print('String size is ${a.length}');
+  var a;
+  a = 10;
+  print(a.runtimeType);
+  a = 'Bhargav';
+  print(a.runtimeType);
+  a = true;
+  print(a.runtimeType);
 }
