b37a5a25fef3081ab51cd3795a216fb971c32710
Bhargav <bhargav.bhavani@odaninfotech.com>
2022-06-10 11:11:20 +0530

    2

--- a1.dart
+++ a1.dart
@@ -1,6 +1,12 @@
-void main() {
-  //double to string
+// void main() {
+//double to string
+//
+//   String b = 50.223.toString();
+//   print(b);
+// }
 
-  String b = 50.223.toString();
-  print(b);
+//String interpolation
+void main() {
+  String a = 'Bhargav';
+  print('String size is ${a.length}');
 }
